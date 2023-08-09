e7bb97b41cd6f1b378b2d6ae10e0d548d9ca62df
ilya <theilyashulepov@gmail.com>
2023-08-09 19:21:54 +0300

    feat: toggle nav mobile

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ const sliderLeft = document.querySelector(".slider--left");
 const sliderRight = document.querySelector(".slider--right");
 let counter = 0;
 
+const expandNavBtn = document.querySelector(".btn--navexpand");
+const nav = document.querySelector("nav");
+const closeNavBtn = document.querySelector(".btn--navclose");
+
 imgSlides.forEach(function (slide, index) {
     slide.style.left = `${index * 100}%`;
 });
 txtSlides.forEach(function (slide, index) {
     slide.style.left = `${index * 120}%`;
+});
+
+expandNavBtn.addEventListener("click", expandNav);
+closeNavBtn.addEventListener("click", closeNav);
+
+sliderLeft.addEventListener("click", function () {
+    slide(1);
+});
+sliderRight.addEventListener("click", function () {
+    slide(-1);
 });
 
 function slide(direction) {
@@ -24,9 +38,20 @@ function slide(direction) {
         slide.style.transform = `translateX(-${counter * 120}%)`;
     });
 }
-sliderLeft.addEventListener("click", function () {
-    slide(1);
-});
-sliderRight.addEventListener("click", function () {
-    slide(-1);
-});
+
+function expandNav() {
+    nav.style.transform = "translateY(100%)";
+    hide(expandNavBtn);
+}
+
+function closeNav() {
+    nav.style.transform = "translateY(0)";
+    show(expandNavBtn);
+}
+
+function show(elt, display = "block") {
+    elt.style.display = display;
+}
+function hide(elt) {
+    elt.style.display = "none";
+}
